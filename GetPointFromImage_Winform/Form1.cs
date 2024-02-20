f1b40f2622434b71063a67c8f58cacc4ae75a3ec
Windows <vtnduc2382000@gmail.com>
2024-02-20 19:23:16 +0700

    Add project files.

---
+++ GetPointFromImage_Winform/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GetPointFromImage_Winform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            double x=e.X;
+            double y=e.Y;
+            MessageBox.Show("{X}: " + x + "{Y}: " + y);
+        }
+    }
+}
